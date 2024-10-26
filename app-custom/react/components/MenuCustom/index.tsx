59248b38b337e896e64adfa13f79462a4502e332
lucasastinofg <lucas.astino@agenciafg.com.br>
2024-10-26 13:48:59 -0300

    ajustando menu mobile

--- app-custom/react/components/MenuCustom/index.tsx
+++ app-custom/react/components/MenuCustom/index.tsx
@@ -1,6 +1,389 @@
+// import React, { useState } from "react";
+// import { useCssHandles } from "vtex.css-handles";
+// import './style.css'
+
+// type SubmenuLink = {
+//   text: string;
+//   url: string;
+//   hasSubmenu?: boolean;
+//   submenuLinks?: SubmenuLink[];
+//   linkColor?: string;
+//   underline?: boolean;
+// };
+
+// type Banner = {
+//   bannerImage: string;
+//   bannerLink: string;
+//   bannerText: string;
+// };
+
+// type MenuLink = {
+//   __editorItemTitle?: string;
+//   text: string;
+//   url: string;
+//   hasSubmenu: boolean;
+//   submenuLinks?: SubmenuLink[];
+//   banners?: Banner[];
+//   linkColor?: string;
+//   underline?: boolean;
+// };
+
+// interface Props {
+//   menuLinks: MenuLink[];
+// }
+
+// const CSS_HANDLES = [
+//   "menuContainer",
+//   "submenuWrapper",
+//   "wrapper",
+//   "menuItem",
+//   "menuLink",
+//   "submenuContainer",
+//   "submenuItem",
+//   "bannerContainer",
+//   "bannerLink",
+//   "bannerText",
+//   "bannerImage",
+//   "submenuToggleIcon",
+//   "activeMenuItem",
+//   "hoverMenuItem",
+//   "openSubmenu",
+//   "level-",
+// ] as const;
+
+// export const AstinoMenu = (props: Props) => {
+//   const { menuLinks } = props;
+//   console.log("menuLinks props:", menuLinks); // Verifique os dados recebidos
+
+//   const [openSubmenus, setOpenSubmenus] = useState<string[]>([]);
+//   const { handles } = useCssHandles(CSS_HANDLES);
+
+//   const isMobile = window.innerWidth <= 768;
+
+//   const handleSubmenuToggle = (indexPath: string) => {
+//     setOpenSubmenus((prevOpenSubmenus) =>
+//       prevOpenSubmenus.includes(indexPath)
+//         ? prevOpenSubmenus.filter((path) => path !== indexPath)
+//         : [...prevOpenSubmenus, indexPath]
+//     );
+//   };
+
+//   const renderSubmenu = (
+//     submenuLinks: SubmenuLink[] | undefined,
+//     level: number = 0,
+//     parentIndexPath: string = ""
+//   ) => {
+//     if (!submenuLinks) return null;
+
+//     return (
+//       <ul
+//         className={`${handles.submenuWrapper} ${handles["level-"]}${level} ${
+//           openSubmenus.includes(parentIndexPath) ? handles.openSubmenu : ""
+//         }`}
+//       >
+//         {submenuLinks.map((submenuLink, subIndex) => {
+//           const currentPath = `${parentIndexPath}-${subIndex}`;
+
+//           return (
+//             <li
+//               key={currentPath}
+//               className={`${handles.submenuItem} ${
+//                 openSubmenus.includes(currentPath) ? handles.activeMenuItem : ""
+//               }`}
+//               onMouseEnter={() =>
+//                 !isMobile &&
+//                 setOpenSubmenus((prevOpenSubmenus) =>
+//                   prevOpenSubmenus.includes(currentPath)
+//                     ? prevOpenSubmenus
+//                     : [...prevOpenSubmenus, currentPath]
+//                 )
+//               }
+//               onMouseLeave={() =>
+//                 !isMobile &&
+//                 setOpenSubmenus((prevOpenSubmenus) =>
+//                   prevOpenSubmenus.filter((path) => path !== currentPath)
+//                 )
+//               }
+//             >
+//               {isMobile ? (
+//               <a
+//               href={submenuLink.url}
+//                   style={{ color: submenuLink.linkColor || "#000" }}
+//                   className={handles.menuLink}
+//                   onClick={(e) => {
+//                     if (submenuLink.hasSubmenu) {
+//                       e.preventDefault();
+//                       handleSubmenuToggle(currentPath);
+//                     }
+//                   }}
+//                   aria-controls={`submenu-${currentPath}`}
+//                   aria-expanded={
+//                     openSubmenus.includes(currentPath) ? "true" : "false"
+//                   }
+//                 >
+//                   {submenuLink.text}
+//                   {submenuLink.hasSubmenu && (
+//                     <span
+//                       className={`${handles.submenuToggleIcon} ${
+//                         openSubmenus.includes(currentPath) ? "active" : ""
+//                       }`}
+//                     />
+//                   )}
+//                 </a>
+//               ) : (
+//                 <a
+//                   href={submenuLink.url}
+//                   style={{ color: submenuLink.linkColor || "#000" }}
+//                   className={`${handles.menuLink} ${submenuLink.underline ? "underline" : ""}`}
+//                 >
+//                   {submenuLink.text}
+//                 </a>
+//               )}
+//               {submenuLink.hasSubmenu &&
+//                 renderSubmenu(submenuLink.submenuLinks, level + 1, currentPath)}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     );
+//   };
+
+//   return (
+//     <nav
+//       className={handles.menuContainer}
+//       role="navigation"
+//       aria-label="Main Menu"
+//     >
+//       <ul className={handles.wrapper}>
+//         {menuLinks.map((link, index) => {
+//           const indexPath = `${index}`;
+
+//           return (
+//             <li
+//               key={indexPath}
+//               className={`${handles.menuItem} ${
+//                 openSubmenus.includes(indexPath) ? handles.activeMenuItem : ""
+//               } ${!isMobile ? handles.hoverMenuItem : ""}`}
+//               onMouseEnter={() => !isMobile && setOpenSubmenus([indexPath])}
+//               onMouseLeave={() => !isMobile && setOpenSubmenus([])}
+//               aria-haspopup={link.hasSubmenu ? "true" : undefined}
+//               aria-expanded={
+//                 openSubmenus.includes(indexPath) ? "true" : "false"
+//               }
+//             >
+//               {isMobile ? (
+//                 <a
+//                 href={link.url}
+//                   className={handles.menuLink}
+//                   style={{ color: link.linkColor || "#000" }}
+//                   onClick={(e) => {
+//                     if (link.hasSubmenu) {
+//                       e.preventDefault();
+//                       handleSubmenuToggle(indexPath);
+//                     }
+//                   }}
+//                   aria-controls={`submenu-${indexPath}`}
+//                   aria-expanded={
+//                     openSubmenus.includes(indexPath) ? "true" : "false"
+//                   }
+//                 >
+//                   {link.text}
+//                   {link.hasSubmenu && (
+//                     <span
+//                       className={`${handles.submenuToggleIcon} ${
+//                         openSubmenus.includes(indexPath) ? "guessbr-agenciafg-custom-0-x-active" : ""
+//                       }`}
+//                     />
+//                   )}
+//                 </a>
+//               ) : (
+//                 <a
+//                   href={link.url}
+//                   className={`${handles.menuLink} ${link.underline ? "underline" : ""}`}
+//                   style={{ color: link.linkColor || "#000" }}
+//                 >
+//                   {link.text}
+//                 </a>
+//               )}
+//               {link.hasSubmenu && (
+//                 <div
+//                   id={`submenu-${indexPath}`}
+//                   className={`${handles.submenuContainer} ${
+//                     openSubmenus.includes(indexPath) ? handles.openSubmenu : ""
+//                   }`}
+//                   role="region"
+//                   aria-label={`${link.text} submenu`}
+//                   style={{
+//                     display: openSubmenus.includes(indexPath) ? "flex" : "none",
+//                   }}
+//                 >
+//                   {renderSubmenu(link.submenuLinks, 1, indexPath)}
+
+//                   {link.banners && link.banners.length > 0 && (
+//                     <div className={handles.bannerContainer}>
+//                       {link.banners.map((banner, bannerIndex) => {
+
+//                         return (
+//                           <a
+//                             className={handles.bannerLink}
+//                             key={bannerIndex}
+//                             href={banner.bannerLink}
+//                             target="_blank"
+//                             rel="noopener noreferrer"
+//                           >
+//                             <img
+//                               src={banner.bannerImage}
+//                               alt={`Banner ${bannerIndex + 1}`}
+//                               className={handles.bannerImage}
+//                             />
+
+//                             {banner.bannerText && (
+//                               <span className={handles.bannerText}>{banner.bannerText}</span>
+//                             )}
+//                           </a>
+//                         );
+//                       })}
+//                     </div>
+//                   )}
+//                 </div>
+//               )}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// AstinoMenu.schema = {
+//   title: "Menu Customizado",
+//   description: "Um menu customizado com links dinâmicos e banners nos submenus",
+//   type: "object",
+//   properties: {
+//     menuLinks: {
+//       type: "array",
+//       title: "Links do Menu",
+//       items: {
+//         type: "object",
+//         properties: {
+//           __editorItemTitle: {
+//             type: "string",
+//             title: "Título no Editor",
+//           },
+//           text: {
+//             type: "string",
+//             title: "Texto do Link",
+//             default: "Link",
+//           },
+//           url: {
+//             type: "string",
+//             title: "URL do Link",
+//             default: "#",
+//           },
+//           underline: {
+//             type: "boolean",
+//             title: "Ativar underline nos links?",
+//             default: true,
+//           },
+//           hasSubmenu: {
+//             type: "boolean",
+//             title: "Este link possui um submenu?",
+//             default: false,
+//           },
+//           submenuLinks: {
+//             type: "array",
+//             title: "Links do Submenu",
+//             items: {
+//               type: "object",
+//               properties: {
+//                 __editorItemTitle: {
+//                   type: "string",
+//                   title: "Título no Editor",
+//                 },
+//                 text: {
+//                   type: "string",
+//                   title: "Texto do Link",
+//                   default: "Submenu Link",
+//                 },
+//                 url: {
+//                   type: "string",
+//                   title: "URL do Link",
+//                   default: "#",
+//                 },
+//                 hasSubmenu: {
+//                   type: "boolean",
+//                   title: "Este link possui outro submenu?",
+//                   default: false,
+//                 },
+//                 submenuLinks: {
+//                   type: "array",
+//                   title: "Links do Submenu Aninhado",
+//                   items: {
+//                     type: "object",
+//                     properties: {
+//                       text: {
+//                         type: "string",
+//                         title: "Texto do Link",
+//                         default: "Submenu Link",
+//                       },
+//                       url: {
+//                         type: "string",
+//                         title: "URL do Link",
+//                         default: "#",
+//                       },
+//                     },
+//                   },
+//                 },
+//               },
+//             },
+//           },
+//           banners: {
+//             type: "array",
+//             title: "Banners",
+//             items: {
+//               type: "object",
+//               properties: {
+//                 bannerImage: {
+//                   type: "string",
+//                   title: "URL da Imagem do Banner",
+//                   widget: {
+//                     "ui:widget": "image-uploader",
+//                   },
+//                   default: "",
+//                 },
+//                 bannerLink: {
+//                   type: "string",
+//                   title: "URL do Link do Banner",
+//                   default: "#",
+//                 },
+//                 bannerText: {
+//                   type: "string",
+//                   title: "Texto do banner",
+//                   default: "",
+//                 },
+//               },
+//             },
+//           },
+//           linkColor: {
+//             type: "string",
+//             title: "Cor dos Links",
+//             default: "#000000",
+//             widget: {
+//               "ui:widget": "color",
+//             },
+//           },
+//         },
+//       },
+//       minItems: 1,
+//       addItemText: "Adicionar novo link",
+//       deleteItemText: "Remover link",
+//     },
+//   },
+// };
+
 import React, { useState } from "react";
 import { useCssHandles } from "vtex.css-handles";
-import './style.css'
+import "./style.css";
 
 type SubmenuLink = {
   text: string;
@@ -83,7 +466,7 @@ export const AstinoMenu = (props: Props) => {
       >
         {submenuLinks.map((submenuLink, subIndex) => {
           const currentPath = `${parentIndexPath}-${subIndex}`;
-
+    
           return (
             <li
               key={currentPath}
@@ -106,34 +489,59 @@ export const AstinoMenu = (props: Props) => {
               }
             >
               {isMobile ? (
-                <p
-                  style={{ color: submenuLink.linkColor || "#000" }}
-                  className={handles.menuLink}
-                  onClick={(e) => {
-                    if (submenuLink.hasSubmenu) {
-                      e.preventDefault();
-                      handleSubmenuToggle(currentPath);
+                level >= 2 ? (
+                  <a
+                    href={submenuLink.url}
+                    style={{ color: submenuLink.linkColor || "#000" }}
+                    className={handles.menuLink}
+                    onClick={(e) => {
+                      if (submenuLink.hasSubmenu) {
+                        e.preventDefault();
+                        handleSubmenuToggle(currentPath);
+                      }
+                    }}
+                    aria-controls={`submenu-${currentPath}`}
+                    aria-expanded={
+                      openSubmenus.includes(currentPath) ? "true" : "false"
                     }
-                  }}
-                  aria-controls={`submenu-${currentPath}`}
-                  aria-expanded={
-                    openSubmenus.includes(currentPath) ? "true" : "false"
-                  }
-                >
-                  {submenuLink.text}
-                  {submenuLink.hasSubmenu && (
-                    <span
-                      className={`${handles.submenuToggleIcon} ${
-                        openSubmenus.includes(currentPath) ? "active" : ""
-                      }`}
-                    />
-                  )}
-                </p>
+                  >
+                    {submenuLink.text}
+                    {submenuLink.hasSubmenu && (
+                      <span
+                        className={`${handles.submenuToggleIcon} ${
+                          openSubmenus.includes(currentPath) ? "active" : ""
+                        }`}
+                      />
+                    )}
+                  </a>
+                ) : (
+                  <p
+                    style={{ color: submenuLink.linkColor || "#000" }}
+                    className={handles.menuLink}
+                    onClick={(e) => {
+                      if (submenuLink.hasSubmenu) {
+                        e.preventDefault();
+                        handleSubmenuToggle(currentPath);
+                      }
+                    }}
+                  >
+                    {submenuLink.text}
+                    {submenuLink.hasSubmenu && (
+                      <span
+                        className={`${handles.submenuToggleIcon} ${
+                          openSubmenus.includes(currentPath) ? "active" : ""
+                        }`}
+                      />
+                    )}
+                  </p>
+                )
               ) : (
                 <a
                   href={submenuLink.url}
                   style={{ color: submenuLink.linkColor || "#000" }}
-                  className={`${handles.menuLink} ${submenuLink.underline ? "underline" : ""}`}
+                  className={`${handles.menuLink} ${
+                    submenuLink.underline ? "underline" : ""
+                  }`}
                 >
                   {submenuLink.text}
                 </a>
@@ -145,6 +553,7 @@ export const AstinoMenu = (props: Props) => {
         })}
       </ul>
     );
+    
   };
 
   return (
@@ -171,8 +580,7 @@ export const AstinoMenu = (props: Props) => {
               }
             >
               {isMobile ? (
-                <a
-                href={link.url}
+                <p
                   className={handles.menuLink}
                   style={{ color: link.linkColor || "#000" }}
                   onClick={(e) => {
@@ -190,15 +598,19 @@ export const AstinoMenu = (props: Props) => {
                   {link.hasSubmenu && (
                     <span
                       className={`${handles.submenuToggleIcon} ${
-                        openSubmenus.includes(indexPath) ? "guessbr-agenciafg-custom-0-x-active" : ""
+                        openSubmenus.includes(indexPath)
+                          ? "guessbr-agenciafg-custom-0-x-active"
+                          : ""
                       }`}
                     />
                   )}
-                </a>
+                </p>
               ) : (
                 <a
                   href={link.url}
-                  className={`${handles.menuLink} ${link.underline ? "underline" : ""}`}
+                  className={`${handles.menuLink} ${
+                    link.underline ? "underline" : ""
+                  }`}
                   style={{ color: link.linkColor || "#000" }}
                 >
                   {link.text}
@@ -221,7 +633,6 @@ export const AstinoMenu = (props: Props) => {
                   {link.banners && link.banners.length > 0 && (
                     <div className={handles.bannerContainer}>
                       {link.banners.map((banner, bannerIndex) => {
-
                         return (
                           <a
                             className={handles.bannerLink}
@@ -237,7 +648,9 @@ export const AstinoMenu = (props: Props) => {
                             />
 
                             {banner.bannerText && (
-                              <span className={handles.bannerText}>{banner.bannerText}</span>
+                              <span className={handles.bannerText}>
+                                {banner.bannerText}
+                              </span>
                             )}
                           </a>
                         );
@@ -379,3 +792,4 @@ AstinoMenu.schema = {
     },
   },
 };
+
